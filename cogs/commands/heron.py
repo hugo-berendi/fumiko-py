9f30372946f76a80931d8359cd5390e269a4c66c
hugo-berendi <hugo.berendi@outlook.de>
2022-10-12 15:53:04 +0200

    .

--- cogs/commands/heron.py
+++ cogs/commands/heron.py
@@ -9,20 +9,18 @@ class Heron(commands.Cog):
 		self.bot = bot
 
 	@slash_command(description="Calcs a root with the heron method", name="heron")
-	async def info(self, ctx: discord.ApplicationContext, n: Option(str, "Please enter the number you want the root from")):
+	async def info(self, ctx: discord.ApplicationContext, n: Option(float, "Please enter the number you want the root from"), error: Option(float, "Please enter the error you want to have for the root")):
 
 		def heron(n: float, error: float):
-			prev = 1.0
-			new = (1+n)/2
-			while abs(new-prev) > error:
-				prev = new
-				new = (new+n/new)/2
-			return str(new)
+			y = (n + n / n) / 2
+			while abs(y - n / y) > error:
+				y = (y + n / y) / 2
+			return y
 
 		# create embed named emb
 		emb = discord.Embed(
 			title="Graph",
-			description=heron(n, 0.01),
+			description=f"```\n{heron(n, error)}\n```",
 			color=discord.Color.blue())
 
 		# add timestamp to emb
