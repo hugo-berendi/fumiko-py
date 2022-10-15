b2da35e6232078e0ed2a6507c0fd9eb286533bf5
hugo-berendi <hugo.berendi@outlook.de>
2022-10-15 11:42:20 +0200

    .

--- cogs/commands/math.py
+++ cogs/commands/math.py
@@ -3,14 +3,16 @@ import numpy as np
 import os
 import discord
 from discord.ext import commands
-from discord.commands import slash_command, Option
+from discord.commands import OptionChoice, Option
 
 
-class Graph(commands.Cog):
+class Math(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(description="Calcs a graph", name="graph")
+    math = discord.SlashCommandGroup('math', 'Mathmatics related commands')
+
+    @math.command(description="Calcs a graph", name="graph")
     async def info(self, ctx: discord.ApplicationContext,
                    function: Option(str, "Please enter a function for the Graph (syntax: x**2)")):
 
@@ -42,6 +44,26 @@ class Graph(commands.Cog):
 
             os.remove("out.png")
 
+    @math.command(description="Calcs a root with the heron method", name="heron")
+    async def info(self, ctx: discord.ApplicationContext,
+                   n: Option(int, "Please enter the number you want the root from"),
+                   error: Option(int, "Please enter the error you want to have for the root")):
+        def heron(n: float, error: float):
+            y = (n + n / n) / 2
+            while abs(y - n / y) > error:
+                y = (y + n / y) / 2
+            return y
+
+        # create embed named emb
+        emb = discord.Embed(
+            title="Root with heron method",
+            description=f"```\n{heron(n, error)}\n```",
+            color=discord.Color.blue())
+
+        # add timestamp to emb
+        emb.timestamp = discord.utils.utcnow()
+
+        await ctx.respond(embed=emb)
 
 def setup(bot):
-    bot.add_cog(Graph(bot))
+    bot.add_cog(Math(bot))
