af17e8f9b508416d3aa2ff631194d3fab491f9d6
hugo-berendi <hugo.berendi@outlook.de>
2022-10-15 16:59:30 +0200

    .

--- cogs/commands/testd2.py
+++ cogs/commands/testd2.py
@@ -1,4 +1,4 @@
-import pydest
+import aiobungie
 import discord
 from discord.ext import commands
 from discord.commands import OptionChoice, Option, slash_command
@@ -15,10 +15,27 @@ class Testd2(commands.Cog):
         api_key = '22b29c4bd9a649bfab415322dcca1ec4'
         oauth_url = 'https://www.bungie.net/en/OAuth/Authorize'
         oauth_client_id = 41783
-        destiny = pydest.Pydest(api_key)
-        json = await destiny.api.search_destiny_player(1, 'slayer117')
-        await ctx.respond(json)
-        await destiny.close()
+        client = aiobungie.Client(api_key)
+        async def main() -> None:
+
+        # Fetch a charatcer with all its components.
+        # This includes Equimpents, Inventory, Records, etc.
+        async with client.rest:
+            my_warlock = await client.fetch_character(
+                1,
+                aiobungie.MembershipType.STEAM,
+                1,
+                components=[aiobungie.Component.ALL_CHARACTERS]
+            )
+
+            for activity in my_warlock.activities:
+                # Check if activity is a raid.
+                if activity.current_mode and activity.current_mode is aiobungie.GameMode.RAID:
+                    ctx.channel.send(activity.avaliable_activities) # All raids for this character.
+
+        # You can either run it using the client or just asyncio.run(main())
+        client.run(main())
+        ctx.respond('suc')
 
 def setup(bot):
     bot.add_cog(Testd2(bot))
