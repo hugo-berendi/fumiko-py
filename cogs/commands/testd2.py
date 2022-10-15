2fe043fc3da80d7777685c69246c65d5a2ec9588
hugo-berendi <hugo.berendi@outlook.de>
2022-10-15 16:51:24 +0200

    .

--- cogs/commands/testd2.py
+++ cogs/commands/testd2.py
@@ -12,7 +12,9 @@ class Testd2(commands.Cog):
     async def testd2(
             self,
             ctx: discord.ApplicationContext):
-        api_key = '5601a322825c4a75841a3a5f6678'
+        api_key = '22b29c4bd9a649bfab415322dcca1ec4'
+        oauth_url = 'https://www.bungie.net/en/OAuth/Authorize'
+        oauth_client_id = 41783
         destiny = pydest.Pydest(api_key)
         json = await destiny.api.search_destiny_player(1, 'slayer117')
         await ctx.respond(json)
