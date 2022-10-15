e34564553aa59549793156e7bfc113c1d4219a45
hugo-berendi <hugo.berendi@outlook.de>
2022-10-15 16:44:43 +0200

    .

--- cogs/commands/testd2.py
+++ cogs/commands/testd2.py
@@ -1,4 +1,4 @@
-import requests
+import pydest
 import discord
 from discord.ext import commands
 from discord.commands import OptionChoice, Option, slash_command
@@ -12,13 +12,11 @@ class Testd2(commands.Cog):
     async def testd2(
             self,
             ctx: discord.ApplicationContext):
-        userId = 1
-        url = f"https://www.bungie.net/Platform/User/GetBungieNetUserById/{userId}/"
-        myobjs = {'X-API-Key': '5601a322825c4a75841a3a5f6678'}
-        r1 = requests.post(url, headers=myobjs)
-        await ctx.respond(r1.json())
-
-
+        api_key = '5601a322825c4a75841a3a5f6678'
+        destiny = pydest.Pydest(api_key)
+        json = await destiny.api.search_destiny_player(1, 'slayer117')
+        await ctx.respond(json)
+        await destiny.close()
 
 def setup(bot):
     bot.add_cog(Testd2(bot))
