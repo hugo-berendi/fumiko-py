d9151e4930ce45a53ddc27b92ba405ed30cc8254
hugo-berendi <hugo.berendi@outlook.de>
2022-10-17 15:33:54 +0200

    .

--- cogs/events/ai_chat_bot.py
+++ cogs/events/ai_chat_bot.py
@@ -4,22 +4,24 @@ from discord.ext import commands
 
 
 class Chatbot(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: discord.Bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.channel.id != 991000357842849833:
+        if self.bot.user == message.author:
             return
+        
+        if message.channel.id == 991000357842849833:
+            api_key = "i2JPF619bGn1"
+            rs = RandomStuff(api_key=api_key, async_mode=True)
 
-        api_key = 'i2JPF619bGn1'
-        rs = RandomStuff(api_key=api_key)
-        input = message.content
-        res = rs.get_ai_response(input)
+            input = message.content
 
-        await message.channel.send(res)
+            res = await rs.get_ai_response(input)
+            await message.reply(res)
 
-        rs.close()
+            rs.close()
 
 
 def setup(bot):
