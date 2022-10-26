c9330286e537155c51dd5c88663170fa03373423
hugo-berendi <hugo.berendi@outlook.de>
2022-10-26 18:59:56 +0200

    .

--- cogs/commands/dnd.py
+++ cogs/commands/dnd.py
@@ -1,4 +1,4 @@
-import os
+from os import environ
 import discord
 import pymongo
 import random
@@ -12,115 +12,48 @@ load_dotenv()
 
 class Player:
     def __init__(self,
-                 player_id: int,
+                 id: int,
                  name: str,
-                 background: str,
+                 age: int,
                  group: str,
-                 race: str,
-                 hp: float,
-                 lvl: int,
-                 ep: float,
+                 attribute: str,
+                 description: str,
+                 skills: list,
                  stats: dict):
-        self.player_id = player_id
+        self.id = id
         self.name = name
-        self.background = background
+        self.age = age
         self.group = group
-        self.race = race
-        self.hp = hp
-        self.lvl = lvl
+        self.attribute = attribute
+        self.description = description
+        self.skills = skills
         self.ep = ep
         self.stats = stats
-        self.dead = False
 
-    def getDmg(self, dmg: float):
-        self.hp = self.hp - dmg
+    def getDmg(self, dmg: int):
+        self.stats.health = self.stats.health - dmg
         if self.hp <= 0:
-            self.hp = 0
-            self.dead = True
+            self.stats.health = 0
             return 'you are dead'
         else:
             hp = self.hp
             return hp
 
-    def getEp(self, ep: int):
-        self.ep = self.ep + ep
-        match ep:
-            case 0:
-                return
-            case 300:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 900:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 2700:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 6500:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 14000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 23000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 34000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 48000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 85000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 100000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 120000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 140000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 165000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 195000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 225000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 265000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 305000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case 355000:
-                self.lvl = self.lvl + 1
-                return 'Level UP!!!'
-            case _:
-                return self.ep
-
     def export(self):
         dnd_char = {
-            '_id': self.player_id,
+            '_id': self.id,
             'name': self.name,
-            'background': self.background,
+            'age': self.age,
             'group': self.group,
-            'race': self.race,
-            'hp': self.hp,
-            'lvl': self.lvl,
-            'ep': self.ep,
-            'stats': self.stats,
-            'dead': self.dead
+            'attribute': self.attribute,
+            'description': self.description,
+            'skills': self.skills,
+            'stats': self.stats
         }
         return dnd_char
 
 
-class DnD_Game:
+class Game:
     def __init__(self, player: Player):
         self.player = player
 
@@ -139,22 +72,63 @@ class DnD_Game:
 
         enemie = random.choice(enemies)
         dmg = enemie_stats[str(enemie)]['dmg']
-        self.player.ep = self.player.ep + 100
-        return self.player.getDmg(float(dmg))
+        return self.player.getDmg(int(dmg))
+
+
+class PlayerInfoEmbed:
+    def __init__(self, player: Player):
+        self.p = player
+
+    def send(self):
+        def skills(player: Player):
+            skills = ""
+            for skill in player.export()['skills']:
+                if skill['lvl'] == None:
+                    skills += f"{skill['name']}\n"
+                else:
+                    skills += f"{skill['name']} Lv. {skill['lvl']}\n"
+            return skills
+
+        emb = discord.Embed(
+            title="[Character Information]",
+            description="",
+            color=ctx.author.color,
+            fields=[
+                discord.EmbedField(name='Name', value=p.export()['name'], inline=True),
+                discord.EmbedField(name='Age', value=f"{p.export()['age']} years old.", inline=True),
+                discord.EmbedField(name='Group', value=p.export()['group'], inline=True),
+                discord.EmbedField(name='Description', value=p.export()['description'], inline=False),
+                discord.EmbedField(name='Attribute', value=f"{p.export()['attribute']}", inline=False),
+                discord.EmbedField(name='Skills', value=f"{skills(self.p)}", inline=False),
+                discord.EmbedField(name='Overall Stats',
+                                   value=f"""
+                                   Health:         Lv. {p.export()["stats"]["health"]}
+                                   Stamina:        Lv. {p.export()["stats"]["stamina"]}
+                                   Strength:       Lv. {p.export()["stats"]["strength"]}
+                                   Agility:        Lv. {p.export()["stats"]["agility"]}
+                                   Magic Power:    Lv. {p.export()["stats"]["magic_power"]}
+                                   """,
+                                   inline=False)
+            ],
+            timestamp=discord.utils.utcnow())
+        emb.set_footer(text="Bot by: Kamachi#2491")
 
 
 class Dnd(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    dnd = discord.SlashCommandGroup('dnd', 'Dungeons & Dragons related commands')
+    dnd = discord.SlashCommandGroup('dnd', 'Death & Dungeons related commands')
 
     @dnd.command(name="create", description="Create your dnd character")
     async def create(self,
                      ctx: discord.ApplicationContext,
                      name: Option(str, "Please input the name of your character."),
-                     background: Option(str, 'Please write a background for your character.'),
-                     character_class: Option(
+                     age: Option(int, 'Please input the age of your character.'),
+                     attribute: Option(str,
+                                       'Please input the peronality/attribute of your character. ("Cold hearted (General)")'),
+                     description: Option(str, 'Please write a background for your character.'),
+                     group: Option(
                          str,
                          description='Choose the class of your character.',
                          choices=[
@@ -170,164 +144,31 @@ class Dnd(commands.Cog):
                              OptionChoice(name='Sorcerer', value='sorcerer'),
                              OptionChoice(name='Warlock', value='warlock'),
                              OptionChoice(name='Wizard', value='wizard')
-                         ]),
-                     character_race: Option(
-                         str,
-                         description='Choose a race for your character.',
-                         choices=[
-                             OptionChoice(name='Dragonborn', value='dragonborn'),
-                             OptionChoice(name='Dwarf', value='dwarf'),
-                             OptionChoice(name='Elf', value='elf'),
-                             OptionChoice(name='Half-Elf', value='half-elf'),
-                             OptionChoice(name='Halfling', value='halfling'),
-                             OptionChoice(name='Half-Orc', value='half-orc'),
-                             OptionChoice(name='Human', value='human'),
-                             OptionChoice(name='Tiefling', value='tiefling'),
                          ])):
         # init mongodb
-        client = pymongo.MongoClient(os.environ.get('MONGOURI'))
+        client = pymongo.MongoClient(environ.get('MONGOURI'))
         db = client['Fumiko']
         dnd_chars = db['dnd_chars']
 
-        def init_dnd_char_group(group):
-            if group == 'barbarian':
-                hp = 13
-                return hp
-            elif group == 'bard':
-                hp = 9
-                return hp
-            elif group == 'cleric':
-                hp = 9
-                return hp
-            elif group == 'druid':
-                hp = 9
-                return hp
-            elif group == 'fighter':
-                hp = 11
-                return hp
-            elif group == 'monk':
-                hp = 9
-                return hp
-            elif group == 'paladin':
-                hp = 11
-                return hp
-            elif group == 'ranger':
-                hp = 11
-                return hp
-            elif group == 'rogue':
-                hp = 9
-                return hp
-            elif group == 'sorcerer':
-                hp = 7
-                return hp
-            elif group == 'warlock':
-                hp = 9
-                return hp
-            elif group == 'wizard':
-                hp = 7
-                return hp
-
-        def init_dnd_char_race(race):
-            if race == 'dragonborn':
-                stats = {
-                    "Strength": 17,
-                    "Dexterity": 14,
-                    "Constitution": 13,
-                    "Intelligence": 12,
-                    "Wisdom": 10,
-                    "Charisma": 9,
-                }
-                return stats
-            elif race == 'dwarf':
-                stats = {
-                    "Strength": 15,
-                    "Dexterity": 14,
-                    "Constitution": 15,
-                    "Intelligence": 12,
-                    "Wisdom": 10,
-                    "Charisma": 8,
-                }
-                return stats
-            elif race == 'elf':
-                stats = {
-                    "Strength": 15,
-                    "Dexterity": 16,
-                    "Constitution": 13,
-                    "Intelligence": 12,
-                    "Wisdom": 10,
-                    "Charisma": 8,
-                }
-                return stats
-            elif race == 'gnome':
-                stats = {
-                    "Strength": 15,
-                    "Dexterity": 14,
-                    "Constitution": 13,
-                    "Intelligence": 14,
-                    "Wisdom": 10,
-                    "Charisma": 8,
-                }
-                return stats
-            elif race == 'half-elf':
-                stats = {
-                    "Strength": 15,
-                    "Dexterity": 15,
-                    "Constitution": 14,
-                    "Intelligence": 12,
-                    "Wisdom": 10,
-                    "Charisma": 10,
-                }
-                return stats
-            elif race == 'halfling':
-                stats = {
-                    "Strength": 15,
-                    "Dexterity": 16,
-                    "Constitution": 13,
-                    "Intelligence": 12,
-                    "Wisdom": 10,
-                    "Charisma": 8,
-                }
-                return stats
-            elif race == 'half-orc':
-                stats = {
-                    "Strength": 17,
-                    "Dexterity": 14,
-                    "Constitution": 14,
-                    "Intelligence": 12,
-                    "Wisdom": 10,
-                    "Charisma": 8,
-                }
-                return stats
-            elif race == 'human':
-                stats = {
-                    "Strength": 16,
-                    "Dexterity": 15,
-                    "Constitution": 14,
-                    "Intelligence": 13,
-                    "Wisdom": 11,
-                    "Charisma": 9,
-                }
-                return stats
-            elif race == 'tiefling':
-                stats = {
-                    "Strength": 15,
-                    "Dexterity": 14,
-                    "Constitution": 13,
-                    "Intelligence": 13,
-                    "Wisdom": 10,
-                    "Charisma": 10,
-                }
-                return stats
-
-        p = Player(player_id=ctx.author.id,
+        p = Player(id=ctx.author.id,
                    name=name,
-                   background=background,
-                   group=character_class,
-                   race=character_race,
-                   hp=init_dnd_char_group(character_class),
-                   lvl=1,
-                   ep=0,
-                   stats=init_dnd_char_race(character_race))
+                   age=age,
+                   group=group,
+                   attribute=attribute,
+                   description=description,
+                   skills=[
+                       {
+                           'name': 'You don\'t have any skill right now',
+                           'lvl': None
+                       }
+                   ],
+                   stats={
+                       'health': 100,
+                       'stamina': 6,
+                       'strength': 6,
+                       'agility': 6,
+                       'magic_power': 6,
+                   })
 
         # cmd actions
         dnd_chars.find_one_and_update(
@@ -340,32 +181,14 @@ class Dnd(commands.Cog):
             upsert=True
         )
 
-        # create embed named emb
-        emb = discord.Embed(
-            title=f"{ctx.author.name}'s character sheet",
-            description="",
-            color=ctx.author.color,
-            fields=[
-                discord.EmbedField(name="Name", value=p.export()["name"], inline=True),
-                discord.EmbedField(name="Class", value=p.export()["group"], inline=True),
-                discord.EmbedField(name="Race", value=p.export()["race"], inline=True),
-                discord.EmbedField(name="Background", value=p.export()["background"], inline=False),
-                discord.EmbedField(name="Level", value=f'{p.export()["lvl"]}', inline=True),
-                discord.EmbedField(name="EP", value=f'{p.export()["ep"]}', inline=True),
-                discord.EmbedField(name='Stats',
-                                   value=f'HP: {p.export()["hp"]}\nStrength: {p.export()["stats"]["Strength"]}\nDexterity: {p.export()["stats"]["Dexterity"]}\nConstitution: {p.export()["stats"]["Constitution"]}\nIntelligence: {p.export()["stats"]["Intelligence"]}\nWisdom: {p.export()["stats"]["Wisdom"]}\nCharisma: {p.export()["stats"]["Charisma"]}',
-                                   inline=False)
-            ],
-            timestamp=discord.utils.utcnow())
-        emb.set_footer(text="Bot by: Kamachi#2491")
+        await ctx.respond(embed=PlayerInfoEmbed(p))
 
-        await ctx.respond(embed=emb)
         client.close()
 
     @dnd.command(name='play', description='Play dnd!')
     async def play(self, ctx: discord.ApplicationContext):
         # init mongodb
-        client = pymongo.MongoClient(os.environ.get('MONGOURI'))
+        client = pymongo.MongoClient(environ.get('MONGOURI'))
         db = client['Fumiko']
         dnd_chars = db['dnd_chars']
 
@@ -375,17 +198,16 @@ class Dnd(commands.Cog):
             }
         )
 
-        p = Player(player_id=ctx.author.id,
+        p = Player(id=ctx.author.id,
                    name=dnd_char['name'],
-                   background=dnd_char['background'],
+                   age=dnd_char['age'],
                    group=dnd_char['group'],
-                   race=dnd_char['race'],
-                   hp=dnd_char['hp'],
-                   lvl=dnd_char['lvl'],
-                   ep=dnd_char['ep'],
+                   attribute=dnd_char['attribute'],
+                   description=dnd_char['description'],
+                   skills=dnd_char['skills'],
                    stats=dnd_char['stats'])
 
-        game = DnD_Game(player=p)
+        game = Game(player=p)
 
         play = game.play()
 
